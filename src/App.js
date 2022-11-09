5e765a75103c64f204c8bb9e51109e003a693e5f
digisidekick <content.digisidekick@gmail.com>
2022-11-09 14:01:56 +0530

    change agyan

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>new one
+          Edit <code>src/App.js</code>this is anuj code
         </p>
         <h1>sjosjdoj</h1>
+        <h1>anuj code </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
